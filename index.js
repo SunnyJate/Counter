0c999a570a632456a401d415f68e34f6c29ef6eb
SunnyJate <sunnyjate2007@gmail.com>
2024-07-15 20:21:01 +0530

    Final Scripting and styling

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function updateCountdown(){
 
     const minutes = Math.floor((timeDifference %(1000*60*60))/(1000*60));
 
-    const seconds = Math.floor((timeDifference / (1000*60)))
+    const seconds = Math.floor((timeDifference % (1000*60))/(1000))
 
-    document.getElementById('countdoun').innerHTML= `${days}d ${hours}h ${minutes}m ${seconds}s`
+    document.getElementById('countdown').innerHTML= `${days}d ${hours}h ${minutes}m ${seconds}s`
 
     setTimeout(updateCountdown,1000)
  }
